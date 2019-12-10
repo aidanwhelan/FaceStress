3752a0de647f49c685b44dd298908bc2eadb9fef
madfain <49881704+madfain@users.noreply.github.com>
2019-12-09 20:08:41 -0500

    Add files via upload

---
+++ Bluefruit_LE_Connect/Notifications/Notifications App/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  Notifications App
+//
+//  Created by Maddie Fain on 12/9/19.
+//  Copyright © 2019 Maddie Fain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ Bluefruit_LE_Connect/Notifications/Notifications App/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Notifications App
+//
+//  Created by Maddie Fain on 12/9/19.
+//  Copyright © 2019 Maddie Fain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
